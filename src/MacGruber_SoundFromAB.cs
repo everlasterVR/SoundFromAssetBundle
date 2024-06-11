5516ed3ebd4eabba45b53607066395d0080d455d
everlaster <everlaster@protonmail.com>
2024-06-11 21:43:37 +0300

    implement PlayPrevious, PlayNext, Autoplay Next; update texts

--- src/MacGruber_SoundFromAB.cs
+++ src/MacGruber_SoundFromAB.cs
@@ -1,30 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using SimpleJSON;
 using Request = MeshVR.AssetLoader.AssetBundleFromFileRequest;
 using MeshVR;
+using MacGruber;
 
-namespace MacGruber
+// LICENSE: CC BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
+// Based on the original SoundFromAB.cs by MacGruber from MacGruber.LogicBricks.14.var
+
+namespace everlaster
 {
-    public class SoundFromAB : MVRScript
-    {
+	public class SoundFromAssetBundle : MVRScript
+	{
 		private JSONStorableUrl myAudioBundle;
 		private JSONStorableBool myAcceptTrigger;
 		private JSONStorableBool myAvoidRepetition;
+		private JSONStorableBool myAutoplayNext;
 		private JSONStorableStringChooser myModeChooser;
 
 		private string myBundleURL = null;
 		private Request myBundleRequest = null;
-		private AudioSourceControl myAudioSource;
+		private AudioSourceControl myAudioSourceControl;
+		private AudioSource myAudioSource;
 
 		private int myModeIndex = DEFAULT_MODE;
 		private static readonly List<string> MODE_NAMES = new List<string>() { "PlayNext", "PlayNextClearQueue", "QueueClip", "PlayNow", "PlayIfClear", "PlayNowLoop", "PlayNowClearQueue" };
 		private const int DEFAULT_MODE = 6;
 
 		private Dictionary<string, Folder> myFolders = new Dictionary<string, Folder>();
+		public static string lastPlayedFolderName;
+		public static bool skipLastClipCheckOnce;
 
 		private class Folder
 		{
+			private string name;
+
+			public Folder(string name)
+			{
+				this.name = name;
+			}
+
 			public void ClearForRebuild()
 			{
 				soundIndex.Clear();
@@ -38,7 +52,7 @@ namespace MacGruber
 				soundIndex.Add(displayName, index);
 			}
 
-			public void Register(SoundFromAB self, string folderName)
+			public void Register(SoundFromAssetBundle self, string folderName)
 			{
 				this.self = self;
 				List<string> displays = new List<string>();
@@ -50,13 +64,19 @@ namespace MacGruber
 				{
 					string singleActionName = string.IsNullOrEmpty(folderName) ? "PlaySpecific" : "PlaySpecific [" + folderName + "]";
 					string randomActionName = string.IsNullOrEmpty(folderName) ? "PlayRandom" : "PlayRandom [" + folderName + "]";
+					string prevActionName = string.IsNullOrEmpty(folderName) ? "PlayPrevious" : "PlayPrevious [" + folderName + "]";
+					string nextActionName = string.IsNullOrEmpty(folderName) ? "PlayNext" : "PlayNext [" + folderName + "]";
 					chooser = new JSONStorableStringChooser(singleActionName, displays, "", "");
 					chooser.isStorable = chooser.isRestorable = false;
 					singleAction = new JSONStorableActionStringChooser(singleActionName, PlaySingleSound, chooser);
 					self.RegisterStringChooserAction(singleAction);
 
 					randomAction = new JSONStorableAction(randomActionName, PlayRandomSound);
+					prevAction = new JSONStorableAction(prevActionName, PlayPreviousSound);
+					nextAction = new JSONStorableAction(nextActionName, PlayNextSound);
 					self.RegisterAction(randomAction);
+					self.RegisterAction(prevAction);
+					self.RegisterAction(nextAction);
 				}
 				else
 				{
@@ -77,7 +97,11 @@ namespace MacGruber
 				singleAction = null;
 
 				self.DeregisterAction(randomAction);
+				self.DeregisterAction(prevAction);
+				self.DeregisterAction(nextAction);
 				randomAction = null;
+				prevAction = null;
+				nextAction = null;
 				return true;
 			}
 
@@ -128,6 +152,22 @@ namespace MacGruber
 				PlayIndexClip(idx);
 			}
 
+			private void PlayPreviousSound()
+			{
+				if (self == null || !self.myAcceptTrigger.val)
+					return;
+
+				PlayIndexClip(currentIdx - 1);
+			}
+
+			private void PlayNextSound()
+			{
+				if (self == null || !self.myAcceptTrigger.val)
+					return;
+
+				PlayIndexClip(currentIdx + 1);
+			}
+
 			private void PlayIndexClip(int idx)
 			{
 				if (idx < 0 || idx >= sounds.Count)
@@ -135,11 +175,12 @@ namespace MacGruber
 
 				myPreviousIndex2 = myPreviousIndex1;
 				myPreviousIndex1 = idx;
+				currentIdx = idx;
 
 				NamedAudioClip clip = sounds[idx];
 				if (self == null || clip == null)
 					return;
-				AudioSourceControl asc = self.myAudioSource;
+				AudioSourceControl asc = self.myAudioSourceControl;
 				if (asc == null)
 					return;
 
@@ -154,19 +195,25 @@ namespace MacGruber
 					case 6: asc.PlayNowClearQueue(clip);  break;
 					default: break;
 				}
+
+				SoundFromAssetBundle.lastPlayedFolderName = name;
+				SoundFromAssetBundle.skipLastClipCheckOnce = true;
 			}
 
 			private JSONStorableStringChooser chooser;
 			private JSONStorableActionStringChooser singleAction;
 			private JSONStorableAction randomAction;
+			private JSONStorableAction prevAction;
+			private JSONStorableAction nextAction;
 			private Dictionary<string, int> soundIndex = new Dictionary<string, int>();
 			private List<NamedAudioClip> sounds = new List<NamedAudioClip>();
+			private int currentIdx = -1;
 			private int myPreviousIndex1 = int.MaxValue;
 			private int myPreviousIndex2 = int.MaxValue;
-			private SoundFromAB self;
+			private SoundFromAssetBundle self;
 		}
 
-        public override void Init()
+		public override void Init()
 		{
 			Utils.OnInitUI(CreateUIElement);
 
@@ -179,9 +226,9 @@ namespace MacGruber
 				storableName = "AptSpeaker_Import";
 			else if (containingAtom.type == "RhythmAudioSource")
 				storableName = "RhythmSource";
-			myAudioSource = containingAtom.GetStorableByID(storableName) as AudioSourceControl;
+			myAudioSourceControl = containingAtom.GetStorableByID(storableName) as AudioSourceControl;
 
-			if (myAudioSource == null)
+			if (myAudioSourceControl == null)
 			{
 				SuperController.LogError("The SoundFromAB plugin needs to be placed on a Person, AudioSource, RhythmAudioSource or AptSpeaker atom.");
 				Utils.SetupInfoTextNoScroll(this,
@@ -191,13 +238,21 @@ namespace MacGruber
 				return;
 			}
 
+			myAudioSource = myAudioSourceControl.audioSource;
+
 			Utils.SetupInfoTextNoScroll(this,
-				"<color=#606060><size=40><b>SoundFromAB</b></size>\nLogicBrick that exposes audio files found in an <i>Unity AssetBundle</i> as <i>Triggers</i>. Folders inside the bundle will be exposed by two triggers each:\n<b><i>PlaySpecific</i></b> and <b><i>PlayRandom</i></b>\nEach trigger includes the folder path in brackets. Example:\n<i>PlaySpecific [MyFolder/Blabla]</i>\nUnder <i>PlaySpecific</i> you find a drop-down menu with the audio files in that folder, while <i>PlayRandom</i> will pick one audio file out of that folder at random.\n\n" +
-				"It's recommended to use file extension <i>*.assetbundle</i> for your <i>AssetBundle</i> files. While other extensions are supported, VaM will not auto-detect those as dependencies when creating a VAR.\n\n" +
-				"Use <b>Avoid Repetition</b> to take the last one or two choices out of the selection, depending on the number of total available choices.\n\n" +
-				"Use <b>Mode</b> to control in which way the next audio should be played. These correspond to the modes offered by regular VaM audio. Can also be set by trigger just before triggering a sound.\n\n" +
-				"Plugin needs to be placed on a Person, AudioSource, RhythmAudioSource or AptSpeaker atom.</color>",
-				1100.0f, true
+				$"<color=#606060><size=40><b>{nameof(SoundFromAssetBundle)}</b></size>" +
+				"\n<i>Based on SoundFromAB by MacGruber</i>" +
+				"\n\nExposes audio files found in a <i>Unity AssetBundle</i> as <i>Triggers</i>. Each folder will have the following triggers actions:" +
+				"\n\n- <b><i>PlaySpecific</i></b> plays the selected clip" +
+				"\n- <b><i>PlayRandom</i></b> picks a random clip" +
+				"\n- <b><i>PlayPrevious</i></b> plays the previous clip" +
+				"\n- <b><i>PlayNext</i></b> plays the next clip" +
+				"\n\nIn addition, the <b>Autoplay Next</b> toggle will automatically trigger PlayNext when a clip ends, until all clips in the folder have been played." +
+				"\n\nIt's recommended to use file extension <i>*.assetbundle</i> for your <i>AssetBundle</i> files. While other extensions are supported, VaM will not auto-detect those as dependencies when creating a VAR." +
+				"\n\nUse <b>Avoid Repetition</b> to discard the last 1-2 choices (depending on the number of total available choices) when using PlayRandom." +
+				"\n\nUse <b>Mode</b> to control in which way the next audio should be played. These correspond to the modes offered by regular VaM audio, and can be set by trigger just before playing a file via one of the above trigger actions.</color>",
+				1200.0f, true
 			);
 
 			myAudioBundle = Utils.SetupAssetBundleChooser(this, "AssetBundle", "", false, "assetbundle|audiobundle|voicebundle");
@@ -214,8 +269,8 @@ namespace MacGruber
 				else
 				{
 					try {
-					Request request = new AssetLoader.AssetBundleFromFileRequest {path = myBundleURL, callback = OnAssetBundleLoaded};
-					AssetLoader.QueueLoadAssetBundleFromFile(request);
+						Request request = new AssetLoader.AssetBundleFromFileRequest {path = myBundleURL, callback = OnAssetBundleLoaded};
+						AssetLoader.QueueLoadAssetBundleFromFile(request);
 					}
 					catch (System.Exception e)
 					{ SuperController.LogError(e.ToString()); }
@@ -224,6 +279,7 @@ namespace MacGruber
 
 			myAcceptTrigger = Utils.SetupToggle(this, "AcceptTrigger", true, false);
 			myAvoidRepetition = Utils.SetupToggle(this, "Avoid Repetition", true, false);
+			myAutoplayNext = Utils.SetupToggle(this, "Autoplay Next", false, false);
 			myModeChooser = Utils.SetupStringChooser(this, "Mode", MODE_NAMES, DEFAULT_MODE, false);
 			myModeChooser.setCallbackFunction += (string v) => {
 				myModeIndex = MODE_NAMES.FindIndex((string entry) => { return entry == v; });
@@ -234,10 +290,10 @@ namespace MacGruber
 				}
 			};
 
-			Utils.SetupAction(this, "StopLoop",          () => { if (myAudioSource != null)	myAudioSource.StopLoop(); });
-			Utils.SetupAction(this, "Stop",              () => { if (myAudioSource != null)	myAudioSource.Stop(); });
-			Utils.SetupAction(this, "StopAndClearQueue", () => { if (myAudioSource != null)	myAudioSource.StopAndClearQueue(); });
-			Utils.SetupAction(this, "ClearQueue",        () => { if (myAudioSource != null)	myAudioSource.ClearQueue(); });
+			Utils.SetupAction(this, "StopLoop",          () => { if (myAudioSourceControl != null)	myAudioSourceControl.StopLoop(); });
+			Utils.SetupAction(this, "Stop",              () => { if (myAudioSourceControl != null)	myAudioSourceControl.Stop(); });
+			Utils.SetupAction(this, "StopAndClearQueue", () => { if (myAudioSourceControl != null)	myAudioSourceControl.StopAndClearQueue(); });
+			Utils.SetupAction(this, "ClearQueue",        () => { if (myAudioSourceControl != null)	myAudioSourceControl.ClearQueue(); });
 		}
 
 		private void OnAssetBundleLoaded(Request aRequest)
@@ -285,7 +341,7 @@ namespace MacGruber
 						Folder folder;
 						if (!myFolders.TryGetValue(folderName, out folder))
 						{
-							folder = new Folder();
+							folder = new Folder(folderName);
 							myFolders.Add(folderName, folder);
 						}
 						folder.AddSound(displayName, clip);
@@ -312,17 +368,47 @@ namespace MacGruber
 			{ SuperController.LogError(e.ToString()); }
 		}
 
+		private AudioClip prevClip;
+
 		private void Update()
 		{
 			if (myBundleURL == null)
 				myAudioBundle.setCallbackFunction(myAudioBundle.val);
+
+			if (!myAutoplayNext.val || myAudioSource == null || lastPlayedFolderName == null)
+				return;
+
+			var clip = myAudioSource.clip;
+			if(skipLastClipCheckOnce)
+			{
+				skipLastClipCheckOnce = false;
+			}
+			else if (this.enabled && myAutoplayNext.val && prevClip != null && prevClip != clip)
+			{
+				if (myFolders.ContainsKey(lastPlayedFolderName))
+				{
+					Debug.Log($"Calling PlayNext on {lastPlayedFolderName}");
+					this.CallAction($"PlayNext [{lastPlayedFolderName}]");
+					skipLastClipCheckOnce = true;
+				}
+			}
+
+			prevClip = clip;
+		}
+
+		private void OnApplicationPause(bool pauseStatus)
+		{
+			if(pauseStatus)
+			{
+				skipLastClipCheckOnce = true;
+			}
 		}
 
 		private void OnDestroy()
 		{
 			if (myBundleURL != null)
-                AssetLoader.DoneWithAssetBundleFromFile(myBundleURL);
+				AssetLoader.DoneWithAssetBundleFromFile(myBundleURL);
 			myBundleURL = null;
 		}
-    }
+	}
 }
